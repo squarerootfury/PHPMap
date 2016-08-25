22318c8f1677e2fa156c07bf5fb4524ff7f15950
Florian Wartner <f.wartner@ployin.co>
2016-08-25 20:26:13 +0200

    Changes

---
+++ app/Http/Controllers/Api/ApiPublic/UserArticleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api\ApiPublic;
+
+use App\User;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class UserArticleController extends Controller
+{
+    public function show($id)
+    {
+        $articles = User::find($id)->blog_entries()->orderBy('created_at', 'desc')->get();
+        
+        return response()->json($articles);
+    }
+}
--- routes/public.php
+++ routes/public.php
@@ -3,7 +3,7 @@
 Route::get('/users', 'Api\Users\UserController@index');
 Route::get('/users/getPosts/{id}', 'Api\ApiPublic\PostController@show');
 
-Route::get('/users/getArticles/{id}', 'Api\ApiPublic\ArticleController@show');
+Route::get('/users/getArticles/{id}', 'Api\ApiPublic\UserArticleController@show');
 
 Route::resource('/meetups', 'Api\ApiPublic\MeetupController@index');
 Route::resource('/articles', 'Api\ApiPublic\ArticleController');
