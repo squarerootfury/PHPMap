592c09544dca84e978c95a8c200555f715774ecd
Florian Wartner <f.wartner@ployin.co>
2016-08-25 20:23:29 +0200

    Changes

--- resources/assets/js/components/SingleUser.vue
+++ resources/assets/js/components/SingleUser.vue
@@ -95,7 +95,14 @@
                     </div>
 
                     <div class="tab-pane fade" id="articles">
-                        <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit.</p>
+
+                        <div class="panel panel-default" v-for="article in userarticles">
+                            <div class="panel-body">
+                                <h4>{{ article.title }}</h4>
+                                <p>{{ article.body | limit }}</p>
+                            </div>
+                        </div>
+
                     </div>
 
                     <div class="tab-pane fade" id="meetups">
@@ -141,13 +148,15 @@
         },
 
         filters: {
-            marked: marked
+            marked: marked,
+            limit: limit
         },
 
         ready() {
             this.getUser();
             this.getPosts();
             this.getUsers();
+            this.getArticles();
         },
 
         methods: {
@@ -159,6 +168,12 @@
                 this.$http.get('/public/users/getPosts/'+ this.singleuser.id).then((response) => {
                     this.userposts = response.json();
                 });
+            },
+
+            getArticles() {
+                this.$http.get('/public/users/getArticles/'+ this.singleuser.id).then((response) => {
+                    this.userarticles = response.json();
+            });
             },
 
             getUsers() {
